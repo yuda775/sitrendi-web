acbedea2933da86a5f907069499f044f61a06404
yuda775 <saputra.yuda775@gmail.com>
2024-05-16 11:28:36 +0700

    initial commit

--- packages/server/netlify/functions/app.js
+++ packages/server/netlify/functions/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
-app.use(morgan());
+app.use(morgan("dev"));
 
 // Routes
 import bookingController from "../../controllers/bookingController.js";
